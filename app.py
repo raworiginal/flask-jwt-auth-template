42e14dedd91f7347c8b1574c39c3719e87dc9c64
raworiginal <randall.a.wolfe@gmail.com>
2025-09-13 10:00:15 -0700

    wrote auth_middleware needs to be tested.

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-# app.py
-# Import the 'Flask' class from the 'flask' library.
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, g
 import psycopg2
 import psycopg2.extras
 from dotenv import load_dotenv
+from auth_middleware import token_required
 import os
 import jwt
 import bcrypt
@@ -48,22 +47,29 @@ def sign_up():
   try:
     new_user_data = request.get_json()
     connection = get_db_connection()
-    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cursor.execute("SELECT * FROM users WHERE username = %s;",
-                   (new_user_data["username"],))
+    cursor = connection.cursor(
+        cursor_factory=psycopg2.extras.RealDictCursor)
+    cursor.execute(
+        "SELECT * FROM users WHERE username = %s;",
+        (new_user_data["username"],)
+    )
     existing_user = cursor.fetchone()
     if existing_user:
       cursor.close()
       return jsonify({"err": "Username already taken"}), 400
     hashed_password = bcrypt.hashpw(
         bytes(new_user_data["password"], 'utf-8'), bcrypt.gensalt())
-    cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s) RETURNING id, username",
-                   (new_user_data["username"], hashed_password.decode('utf-8')))
+    cursor.execute(
+        "INSERT INTO users (username, password) VALUES (%s, %s) RETURNING id, username",
+        (new_user_data["username"],
+         hashed_password.decode('utf-8')))
     created_user = cursor.fetchone()
     connection.commit()
     connection.close()
     # Construct the payload
-    payload = {"username": created_user["username"], "id": created_user["id"]}
+    payload = {
+        "username": created_user["username"],
+        "id": created_user["id"]}
     # Create the token, attaching the payload
     token = jwt.encode({"payload": payload}, os.getenv('JWT_SECRET'))
     # Send the token instead of the user
@@ -77,14 +83,22 @@ def sign_in():
   try:
     sign_in_form_data = request.get_json()
     connection = get_db_connection()
-    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cursor.execute("SELECT * FROM users WHERE username = %s;",
-                   (sign_in_form_data["username"],))
+    cursor = connection.cursor(
+        cursor_factory=psycopg2.extras.RealDictCursor)
+    cursor.execute(
+        "SELECT * FROM users WHERE username = %s;",
+        (sign_in_form_data["username"],)
+    )
     existing_user = cursor.fetchone()
     if existing_user is None:
       return jsonify({"err": "Invalid credentials."}), 401
-    password_is_valid = bcrypt.checkpw(bytes(
-        sign_in_form_data["password"], 'utf-8'), bytes(existing_user["password"], 'utf-8'))
+    password_is_valid = bcrypt.checkpw(
+        bytes(
+            sign_in_form_data["password"],
+            'utf-8'),
+        bytes(
+            existing_user["password"],
+            'utf-8'))
     if not password_is_valid:
       return jsonify({"err": "Invalid credentials."}), 401
     payload = {
@@ -95,6 +109,18 @@ def sign_in():
     return jsonify({"err": str(err)}), 500
   finally:
     connection.close()
+
+
+@app.route("/users")
+def users_index():
+  connection = get_db_connection()
+  cursor = connection.cursor(
+      cursor_factory=psycopg2.extras.RealDictCursor
+  )
+  cursor.execute("SELECT id, username FROM users;")
+  users = cursor.fetchall()
+  connection.close()
+  return jsonify(users), 200
 
 
 if __name__ == "__main__":
