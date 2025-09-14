67a965525a6a9c2670e994cdc27ace4809b529be
raworiginal <randall.a.wolfe@gmail.com>
2025-09-14 08:47:30 -0700

    updated app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, request, g
-import psycopg2
-import psycopg2.extras
 from dotenv import load_dotenv
-from auth_middleware import token_required
 import os
 import jwt
+import psycopg2
+import psycopg2.extras
 import bcrypt
+from auth_middleware import token_required
 
 load_dotenv()
 
@@ -13,33 +13,13 @@ app = Flask(__name__)
 
 
 def get_db_connection():
-  connection = psycopg2.connect(host='localhost',
-                                database='flask_auth_db',
-                                user=os.getenv('POSTGRES_USERNAME'),
-                                password=os.getenv('POSTGRES_PASSWORD'))
+  connection = psycopg2.connect(
+      host='localhost',
+      database='flask_auth_db',
+      user=os.getenv('POSTGRES_USERNAME'),
+      password=os.getenv('POSTGRES_PASSWORD')
+  )
   return connection
-
-
-@app.route("/sign-token", methods=['GET'])
-def sign_token():
-  user = {
-      "id": 1,
-      "username": "test",
-      "password": "test"
-  }
-  token = jwt.encode(user, os.getenv("JWT_SECRET"), algorithm="HS256")
-  return jsonify({"token": token})
-
-
-@app.route('/verify-token', methods=['POST'])
-def verify_token():
-  try:
-    token = request.headers.get('Authorization').split(' ')[1]
-    decoded_token = jwt.decode(token, os.getenv(
-        'JWT_SECRET'), algorithms=["HS256"])
-    return jsonify({"user": decoded_token})
-  except Exception as err:
-    return jsonify({"err": str(err)})
 
 
 @app.route('/auth/sign-up', methods=['POST'])
@@ -47,18 +27,18 @@ def sign_up():
   try:
     new_user_data = request.get_json()
     connection = get_db_connection()
-    cursor = connection.cursor(
-        cursor_factory=psycopg2.extras.RealDictCursor)
-    cursor.execute(
-        "SELECT * FROM users WHERE username = %s;",
-        (new_user_data["username"],)
-    )
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cursor.execute("SELECT * FROM users WHERE username = %s;",
+                   (new_user_data["username"],))
     existing_user = cursor.fetchone()
     if existing_user:
       cursor.close()
       return jsonify({"err": "Username already taken"}), 400
     hashed_password = bcrypt.hashpw(
-        bytes(new_user_data["password"], 'utf-8'), bcrypt.gensalt())
+        bytes(
+            new_user_data["password"],
+            'utf-8'),
+        bcrypt.gensalt())
     cursor.execute(
         "INSERT INTO users (username, password) VALUES (%s, %s) RETURNING id, username",
         (new_user_data["username"],
@@ -66,13 +46,8 @@ def sign_up():
     created_user = cursor.fetchone()
     connection.commit()
     connection.close()
-    # Construct the payload
-    payload = {
-        "username": created_user["username"],
-        "id": created_user["id"]}
-    # Create the token, attaching the payload
+    payload = {"username": created_user["username"], "id": created_user["id"]}
     token = jwt.encode({"payload": payload}, os.getenv('JWT_SECRET'))
-    # Send the token instead of the user
     return jsonify({"token": token}), 201
   except Exception as err:
     return jsonify({"err": str(err)}), 401
@@ -83,12 +58,9 @@ def sign_in():
   try:
     sign_in_form_data = request.get_json()
     connection = get_db_connection()
-    cursor = connection.cursor(
-        cursor_factory=psycopg2.extras.RealDictCursor)
-    cursor.execute(
-        "SELECT * FROM users WHERE username = %s;",
-        (sign_in_form_data["username"],)
-    )
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cursor.execute("SELECT * FROM users WHERE username = %s;",
+                   (sign_in_form_data["username"],))
     existing_user = cursor.fetchone()
     if existing_user is None:
       return jsonify({"err": "Invalid credentials."}), 401
@@ -101,19 +73,31 @@ def sign_in():
             'utf-8'))
     if not password_is_valid:
       return jsonify({"err": "Invalid credentials."}), 401
-    payload = {
-        "username": existing_user["username"], "id": existing_user["id"]}
+    payload = {"username": existing_user["username"], "id": existing_user["id"]}
     token = jwt.encode({"payload": payload}, os.getenv('JWT_SECRET'))
-    return jsonify({"token": token}), 200
+    return jsonify({"token": token}), 201
   except Exception as err:
     return jsonify({"err": str(err)}), 500
   finally:
     connection.close()
 
 
+@app.route('/users')
+@token_required
+def users_index():
+  connection = get_db_connection()
+  cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+  cursor.execute("SELECT id, username FROM users;")
+  users = cursor.fetchall()
+  connection.close()
+  return jsonify(users), 200
+
+
 @app.route('/users/<user_id>')
 @token_required
 def users_index(user_id):
+  # If the user is looking for the details of another user, block the request
+  # Send a 403 status code to indicate that the user is unauthorized
   if user_id != g.user["id"]:
     return jsonify({"err": "Unauthorized"}), 403
   connection = get_db_connection()
